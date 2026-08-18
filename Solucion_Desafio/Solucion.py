146d8a1be8af36508912f0db1c2320cb8cc452ea
Miguel <miguelypkr@gmail.com>
2026-08-18 13:54:08 -0500

    Califacaciones

--- Solucion_Desafio/Solucion.py
+++ Solucion_Desafio/Solucion.py
@@ -1,2 +1,6 @@
 print("¡Bienvenido al sistema de notas!, por favor, ingrese sus notas consecutivamente, cuando es requerido")
-calificacion1 = int(input("¡Ingr"))
+calificacion1 = int(input("¡Ingrese su primera nota!"))
+calificacion2 = int(input("¡Ingrese su segunda nota!"))
+calificacion3 = int(input("¡Ingrese su tercera nota!"))
+calificacion4 = int(input("¡Ingrese su cuarta nota!"))
+calificacion5 = int(input("¡Ingrese su quinta nota!"))
