98ada7965a8119f4054ac0b070f3f96c5126ccdf
Miguel <miguelypkr@gmail.com>
2026-08-18 13:55:30 -0500

    promedio prueba

--- Solucion_Desafio/Solucion.py
+++ Solucion_Desafio/Solucion.py
@@ -4,3 +4,8 @@ calificacion2 = int(input("¡Ingrese su segunda nota!"))
 calificacion3 = int(input("¡Ingrese su tercera nota!"))
 calificacion4 = int(input("¡Ingrese su cuarta nota!"))
 calificacion5 = int(input("¡Ingrese su quinta nota!"))
+
+promedio = (calificacion1 + calificacion2 + calificacion3 + calificacion4 + calificacion5)
+
+if promedio >= 1: 
+    print("ganaste")
