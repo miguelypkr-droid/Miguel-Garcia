e95e3ff1a92aa36e21335f78c484842ab3e103f9
Miguel <miguelypkr@gmail.com>
2026-08-19 07:22:16 -0500

    prueba 4

--- Solucion_Desafio/Solucion.py
+++ Solucion_Desafio/Solucion.py
@@ -1,17 +1,17 @@
 print("¡Bienvenido al sistema de notas!, por favor, ingrese sus notas consecutivamente, cuando es requerido")
-calificacion1 = int(input("¡Ingrese su primera nota!"))
-calificacion2 = int(input("¡Ingrese su segunda nota!"))
-calificacion3 = int(input("¡Ingrese su tercera nota!"))
-calificacion4 = int(input("¡Ingrese su cuarta nota!"))
-calificacion5 = int(input("¡Ingrese su quinta nota!"))
+calificacion1 = float(input("¡Ingrese su primera nota!"))
+calificacion2 = float(input("¡Ingrese su segunda nota!"))
+calificacion3 = float(input("¡Ingrese su tercera nota!"))
+calificacion4 = float(input("¡Ingrese su cuarta nota!"))
+calificacion5 = float(input("¡Ingrese su quinta nota!"))
  
 promedio = (calificacion1 + calificacion2 + calificacion3 + calificacion4 + calificacion5) / 5
 
+print(f"Tu promedio es {promedio}")
+
 if promedio >= 60: 
     print("Aprobado")
-elif 40 <= promedio <= 59:
+elif promedio >= 40:
     print("En recuperacion")
-elif promedio < 40:
-    print("Reprobado")
 else:
-    print("Error, nota invalida")
+    print("Reprobado")
