9749307fda21f6961d4fe1804e5995e5de0395b3
Miguel <miguelypkr@gmail.com>
2026-08-19 07:41:04 -0500

    prueba 5

--- Solucion_Desafio/Solucion.py
+++ Solucion_Desafio/Solucion.py
@@ -1,9 +1,9 @@
 print("¡Bienvenido al sistema de notas!, por favor, ingrese sus notas consecutivamente, cuando es requerido")
-calificacion1 = float(input("¡Ingrese su primera nota!"))
-calificacion2 = float(input("¡Ingrese su segunda nota!"))
-calificacion3 = float(input("¡Ingrese su tercera nota!"))
-calificacion4 = float(input("¡Ingrese su cuarta nota!"))
-calificacion5 = float(input("¡Ingrese su quinta nota!"))
+calificacion1 = float(input("¡Ingrese su primera nota! "))
+calificacion2 = float(input("¡Ingrese su segunda nota! "))
+calificacion3 = float(input("¡Ingrese su tercera nota! "))
+calificacion4 = float(input("¡Ingrese su cuarta nota! "))
+calificacion5 = float(input("¡Ingrese su quinta nota! "))
  
 promedio = (calificacion1 + calificacion2 + calificacion3 + calificacion4 + calificacion5) / 5
 
